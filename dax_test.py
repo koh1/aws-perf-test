48ccfb70e97ef620880c161780758cee27cc430e
koh1 <koh01.amy@gmail.com>
2020-04-28 01:08:04 +0900

    update

--- dax_test.py
+++ dax_test.py
@@ -1,29 +1,29 @@
 import os, sys, time, datetime
 import amazondax
-from botocore.session import Session
+from boto3.session import Session
 import asyncio
 
 p = sys.argv
-session = Session(aws_access_key_id=p[1], aws_secret_access_key=p[2], region_name="ap-northeast-1")
-dynamo = session.create_client('dynamodb', region_name='ap-northeast-1')
-table_name = "perftest"
+if len(p) < 4:
+    print("Usage: python %s <AWS_ID> <AWS_SECRET> <endpoint>" % p[0])
+    sys.exit(0)
 
-if len(p) > 3:
-    endpoint = p[3]
-    dax = amazondax.AmazonDaxClient(session, region_name='ap-northeast-1', endpoints=[endpoint])
-    client = dax
-else:
-    client = dynamo
+session = Session(aws_access_key_id=p[1], aws_secret_access_key=p[2], region_name="ap-northeast-1")
+dynamo = session.resource('dynamodb')
+TABLE_NAME = "perftest"
+key = "key"
+table = dynamo.Table(TABLE_NAME)
+dax = amazondax.AmazonDaxClient(session, region_name='ap-northeast-1', endpoints=[p[3]])
 
 async def get_item(k, wait):
     for i in range(100):
         params = {
-            "TableName": table_name,
+            "TableName": TABLE_NAME,
             "Key": {
                 "key": {"S": "allowed"}
             }
         }
-        client.get_item(**params)
+        dax.get_item(**params)
         await asyncio.sleep(wait)
 
 if __name__ == '__main__':
@@ -37,10 +37,10 @@ if __name__ == '__main__':
     ))
     t1 = datetime.datetime.now()
     params = {
-        "TableName": table_name,
+        "TableName": TABLE_NAME,
         "Key": {
             "key": {"S": "allowed"}
         }
     }
-    print(client.get_item(**params))
+    print(dax.get_item(**params))
     print(t1-t0)
